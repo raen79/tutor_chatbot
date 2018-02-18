88c46da31cd5d3139d74550972a2bfa63c3629c1
Eran Peer <eran.peer79@gmail.com>
2018-02-18 22:13:49 +0000

    used block expectation for error handling rspec

--- spec/lib/semantic_similarity_spec.rb
+++ spec/lib/semantic_similarity_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe SemanticSimilarity do
   describe 'intialize input parameter' do
     describe 'first question' do
       context 'when is less than 3 characters' do
-        subject(:semantic_similarity) { FactoryBot.build :semantic_similarity, first_question: 'a?' }
-        it { is_expected.to raise_error(ArgumentError, 'first_question must be greater than or equal to 3 characters.') }
+        subject { FactoryBot.build :semantic_similarity, first_question: 'a?' }
+        it { expect{ subject }.to raise_error(ArgumentError, 'first_question must be greater than or equal to 3 characters.') }
       end
 
       context 'when is greater than 140 characters' do
@@ -16,19 +16,19 @@ RSpec.describe SemanticSimilarity do
             that should be used by other applications for the same purposes, don't you think?
           HEREDOC
         }
-        subject(:semantic_similarity) { FactoryBot.build :semantic_similarity, first_question: first_question }
+        subject { FactoryBot.build :semantic_similarity, first_question: first_question }
         it { is_expected.to raise_error(ArgumentError, 'first_question must be lower than or equal to 140 characters.') }
       end
 
       context 'when doesn\'t end in a question mark' do
-        subject(:semantic_similarity) { FactoryBot.build :semantic_similarity, first_question: 'Do you know what the value of pi is.' }
+        subject { FactoryBot.build :semantic_similarity, first_question: 'Do you know what the value of pi is.' }
         it { is_expected.to raise_error(ArgumentError, 'first_question must end in a question mark.') }
       end
     end
 
     describe 'second question' do
       context 'when is less than 3 characters' do
-        subject(:semantic_similarity) { FactoryBot.build :semantic_similarity, second_question: 'a?' }
+        subject { FactoryBot.build :semantic_similarity, second_question: 'a?' }
         it { is_expected.to raise_error(ArgumentError, 'second_question must be greater than or equal to 3 characters.') }        
       end
 
@@ -39,12 +39,12 @@ RSpec.describe SemanticSimilarity do
             that should be used by other applications for the same purposes, don't you think?
           HEREDOC
         }
-        subject(:semantic_similarity) { FactoryBot.build :semantic_similarity, second_question: second_question }
+        subject { FactoryBot.build :semantic_similarity, second_question: second_question }
         it { is_expected.to raise_error(ArgumentError, 'second_question must be lower than or equal to 140 characters.') }
       end
 
       context 'when doesn\'t end in a question mark' do
-        subject(:semantic_similarity) { FactoryBot.build :semantic_similarity, second_question: 'Do you know what the value of pi is.' }
+        subject { FactoryBot.build :semantic_similarity, second_question: 'Do you know what the value of pi is.' }
         it { is_expected.to raise_error(ArgumentError, 'second_question must end in a question mark.') }
       end
     end
@@ -53,7 +53,7 @@ RSpec.describe SemanticSimilarity do
   describe 'instance attribute' do
     describe 'score' do
       let(:semantic_similarity) { FactoryBot.build :semantic_similarity }
-      subject(:score) { semantic_similarity.score }
+      subject { semantic_similarity.score }
       it { is_expected.to be_a(Float) }
       it { is_expected.to be >= 0.0 }
       it { is_expected.to be <= 1.0 }
@@ -66,14 +66,14 @@ RSpec.describe SemanticSimilarity do
         context 'and the second question is `Are you aware whether this movie exists not in black and white?`' do
           let(:second_question) { 'Are you aware whether this movie exists not in black and white?' }
           let(:semantic_similarity) { FactoryBot.build :semantic_similarity, first_question: first_question, second_question: second_question }
-          subject(:same_question?) { semantic_similarity.same_question? }
+          subject { semantic_similarity.same_question? }
           it { is_expected.to be true }
         end
 
         context 'and the second question is `Do you know if this book has been written yet?`' do
           let(:second_question) { 'Do you know if this book has been written yet?' }
           let(:semantic_similarity) { FactoryBot.build :semantic_similarity, first_question: first_question, second_question: second_question }
-          subject(:same_question?) { semantic_similarity.same_question? }
+          subject { semantic_similarity.same_question? }
           it { is_expected.to be true }
         end
       end
