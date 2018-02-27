2dc0f24ccf82ebc132ed2ae3c277070961378fa1
Eran Peer <eran.peer79@gmail.com>
2018-02-27 18:04:14 +0000

    removed semantic similarity dependency and updated factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,20 +1,11 @@
-require 'semantic_similarity'
-
 FactoryBot.define do
   factory :synonym do
     word "MyString"
     words ["Word1", "Word2"]
   end
-  
+
   factory :faq do
-    question "MyString"
-    answer "MyString"
-  end
-
-  factory :semantic_similarity, class: SemanticSimilarity do
-    first_question "Do you know if this film is available in color?"
-    second_question "Are you aware whether this movie exists not in black and white?"
-
-    initialize_with { new(first_question, second_question) }
+    question "Do you know if this film is available in color?"
+    answer "Answer"
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,7 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require 'support/factory_bot'
 require 'pry'
+require 'webmock/rspec'
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
@@ -46,6 +47,8 @@ RSpec.configure do |config|
   # inherited by the metadata hash of host groups and examples, rather than
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  WebMock.disable_net_connect!(allow_localhost: true)
 
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
