2d7797eb76bd11d2ab6ff673dc3bbce75fb9eb2e
Eran Peer <eran.peer79@gmail.com>
2018-03-02 01:05:49 +0000

    created question mailer to send questions to lecturers and get answers

--- app/mailers/question_mailer.rb
+++ app/mailers/question_mailer.rb
@@ -1,2 +1,20 @@
 class QuestionMailer < ApplicationMailer
+  def ask_lecturer(student_question)
+    @student_question = student_question
+    lecturer = User.find_by(:lecturer_id => student_question.lecturer_id)
+
+    mail(
+      :from => ENV['GM_USER'],
+      :to => lecturer.email,
+      :subject => "SQ##{student_question.id} | Tutor Chatbot Has a New Question for You"
+    )
+  end
+
+  def self.receive_answer
+    Mail.all.each do |mail|
+      student_question_id = mail.subject[/SQ#(.*?) \| /, 1].to_i
+      student_question = StudentQuestion.find(student_question_id)
+      student_question.answer(mail.decoded)
+    end
+  end
 end
--- spec/mailers/question_mailer_spec.rb
+++ spec/mailers/question_mailer_spec.rb
@@ -3,56 +3,54 @@ require 'rails_helper'
 RSpec.describe QuestionMailer, type: :mailer do
   describe '.ask_lecturer' do
     before(:each) do
-      allow(User).to receive(:find_by).and_raise(ActiveRecord::RecordNotFound)
-      allow(User).to receive(:find_by).with(:lecturer_id => 'C1529373').and_return({ :email => 'peere@cardiff.ac.uk' })
+      allow(User).to receive(:find_by).with(:lecturer_id => 'C1529373').and_return(User.new(:email => 'peere@cardiff.ac.uk'))
     end
 
-    let!(:student_question) { FactoryBot.create :student_question, :id => 510, :lecturer_id => lecturer_id }
     subject { QuestionMailer.ask_lecturer(student_question) }
+    let(:student_question) { FactoryBot.create :student_question, :id => 510, :lecturer_id => lecturer_id }
+    let(:lecturer_id) { 'C1529373' }
+    let(:to) { ['peere@cardiff.ac.uk'] }
+    let(:m_subject) { "SQ##{student_question.id} | Tutor Chatbot Has a New Question for You" }
+    let(:from) { [ENV['GM_USER']] }
 
-    context 'when lecturer email could not be found' do
-      let(:lecturer_id) { 'BLANK' }
-      it { expect { subject }.to raise_error(ActiveRecord::RecordNotFound) }
+    it 'includes appropriate headers' do
+      expect(subject.to).to eq(to)
+      expect(subject.subject).to eq(m_subject)
+      expect(subject.from).to eq(from)
     end
 
-    context 'when lecturer email could be found' do
-      let(:lecturer_id) { 'C1529373' }
-      let(:to) { ['peere@cardiff.ac.uk'] }
-      let(:m_subject) { "SQ##{student_question.id} | Tutor Chatbot Has a New Question for You" }
-      let(:from) { [ENV['GM_USER']] }
-
-      it { is_expected.to have_attributes(:to => to, :subject => m_subject, :from => from) }
-
-      it 'renders the body with module id, coursework id, student question' do
-        expect(subject.body.encoded).to include("Module ID: #{student_question.module_id}")
-        expect(subject.body.encoded).to include("Coursework ID: #{student_question.coursework_id}")
-        expect(subject.body.encoded).to include(student_question.text)
-        expect(subject.body.encoded).to include('Reply to this email without altering its subject or recipients with a plaintext answer to the question.')
-      end
+    it 'renders the body with module id, coursework id, student question' do
+      expect(subject.body.encoded).to include('Module ID:')
+      expect(subject.body.encoded).to include(student_question.module_id)
+      expect(subject.body.encoded).to include('Coursework ID:')
+      expect(subject.body.encoded).to include(student_question.coursework_id)
+      expect(subject.body.encoded).to include(student_question.text)
+      expect(subject.body.encoded).to include('Reply to this email without altering its subject or recipients with a plaintext answer to the question.')
     end
   end
 
   describe '.receive_answer' do
     before(:each) do
-      allow(User).to receive(:find_by).and_raise(ActiveRecord::RecordNotFound)
-      allow(User).to receive(:find_by).with(:email => 'peere@cardiff.ac.uk').and_return({ :lecturer_id => 'C1529373' })
+      allow(User).to receive(:find_by).with(:email => 'peere@cardiff.ac.uk').and_return(User.new(:lecturer_id => 'C1529373'))
+      allow(Datamuse).to receive(:words).and_return([{ 'word' => 'test' }])
       allow(Mail).to receive(:all)
-                 .and_return([{
-                              :to => [ENV['GM_USER']],
-                              :from => ['peere@cardiff.ac.uk'],
-                              :subject => 'Re: SQ#510 | Tutor Chatbot Has a New Question for You',
-                              :body => 'This is the reply to your question.'
+                 .and_return([Mail.new {
+                              to ENV['GM_USER'];
+                              from 'peere@cardiff.ac.uk';
+                              subject 'Re: SQ#510 | Tutor Chatbot Has a New Question for You';
+                              body 'This is the reply to your question.'
                             },
-                            {
-                              :to => [ENV['GM_USER']],
-                              :from => ['peere@cardiff.ac.uk'],
-                              :subject => 'Re: SQ#670 | Tutor Chatbot Has a New Question for You',
-                              :body => 'This is another reply to your question.'
+                            Mail.new {
+                              to ENV['GM_USER'];
+                              from 'peere@cardiff.ac.uk';
+                              subject 'Re: SQ#670 | Tutor Chatbot Has a New Question for You';
+                              body 'This is another reply to your question.'
                             }])
+      
+      FactoryBot.create :student_question, :id => 510, :text => 'different question?', :lecturer_id => 'C1529373'
+      FactoryBot.create :student_question, :id => 670, :lecturer_id => 'C1529373'
     end
 
-    let!(:student_question) { FactoryBot.create :student_question, :id => 510, :lecturer_id => 'C1529373' }
-    let!(:student_question) { FactoryBot.create :student_question, :id => 670, :lecturer_id => 'C1529373' }
     subject { QuestionMailer.receive_answer }
 
     it { expect { subject }.to change { StudentQuestion.count }.by(-2) }
