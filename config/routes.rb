20249f6627ea1f4c9e9632c0b20ee64531c3b807
Eran Peer <eran.peer79@gmail.com>
2018-03-07 12:17:05 +0000

    added rspec to travis and fixed routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   mount Swaggard::Engine, at: '/api_docs/swagger/'
-  
-  scope 'coursework/:coursework_id' do
-    resources :faqs
-    get '/find_answer', :to => 'faqs#find_answer'
+
+  scope 'api' do
+    scope 'coursework/:coursework_id' do
+      resources :faqs
+      get '/find_answer', :to => 'faqs#find_answer'
+    end
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
